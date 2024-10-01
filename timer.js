9ef2635df484808b44accc754c117820f8a717f2
Ahsshalaby <ahsshalaby@gmail.com>
2024-09-30 20:42:21 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -2,33 +2,38 @@ let countdown;
 let time_seconds = 0;
 let isPaused = true; 
 
-function setTimer() {
-  const hours = parseInt(document.getElementById('hours').value);
-  const minutes = parseInt(document.getElementById('minutes').value);
-  const seconds = parseInt(document.getElementById('seconds').value);
+function setTimer() { // take information form index.html
+  let hours = parseInt(document.getElementById('hours').value) ;
+  let minutes = parseInt(document.getElementById('minutes').value) ;
+  let seconds = parseInt(document.getElementById('seconds').value) ;
 
+// math to calculate the time in seconds to make all under same variable
   time_seconds = (hours * 3600) + (minutes * 60) + seconds;
 
+  // while timer is running
   if (time_seconds > 0) {
     document.getElementById('timerMessage').innerText = "Timer set for " + hours + " hr " + minutes + " min " + seconds + " sec";
     document.getElementById('play_pause').src = 'green_play.png';
-    isPaused = true;
     clearInterval(countdown);
   } else {
-    alert("Please set a valid time!");
+    alert("Please set a valid time!"); // if time is 0. 
   }
 }
 
 function toggleTimer() {
+  if (time_seconds == 0){
+    setTimer(); // activates set timer if button was not pressed. Or if user did not set their timer settings
+  }
   if (isPaused && time_seconds > 0) {
-    startTimer();
+    startTimer(); // if timer paused and is running start the timer
   } else {
-    pauseTimer();
+    pauseTimer(); 
   }
 }
 
 function startTimer() {
-  isPaused = false;
+  
+  isPaused = false; // not paused
   document.getElementById('play_pause').src = 'running_timer.png';
   countdown = setInterval(updateTimer, 1000);
 }
@@ -40,7 +45,7 @@ function pauseTimer() {
   document.getElementById('timerMessage').innerText = "Paused at " + formatTime(time_seconds);
 }
 
-function updateTimer() {
+function updateTimer() { // called by start timer function
   if (time_seconds > 0) {
     time_seconds--;
     document.getElementById('timerMessage').innerText = "Countdown: " + formatTime(time_seconds);
